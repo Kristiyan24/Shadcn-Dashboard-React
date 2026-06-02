ead802c05f275411c9dadfbc62ca459696afab67
kristiyan <example@example.bg>
2026-06-02 22:41:00 +0300

    Add AppNavbar and AppSidebar

---
+++ components/common/AppSidebar.tsx
@@ -0,0 +1,96 @@
+import Link from "next/link";
+import { Home, Inbox, Calendar, Search, Settings, User2, ChevronUp, Plus, Projector, ChevronDown } from "lucide-react";
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarSeparator } from "../ui/sidebar";
+import Image from "next/image";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
+const items = [
+  {
+    title: "Home",
+    icon: Home,
+    url: "/",
+  },
+  {
+    title: "Inbox",
+    icon: Inbox,
+    url: "#",
+  },
+  {
+    title: "Calendar",
+    icon: Calendar,
+    url: "#",
+  },
+  {
+    title: "Search",
+    icon: Search,
+    url: "#",
+  },
+  {
+    title: "Settings",
+    icon: Settings,
+    url: "#",
+  },
+];
+
+export default function AppSidebar() {
+    return(
+        <Sidebar collapsible="icon">
+
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton asChild>
+                            <Link href="/">
+                                <Image src="/logo.svg" alt="logo" width={20} height={20}/>
+                                <span>Lama Developer</span>
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+
+            <SidebarSeparator/>
+
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Application</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                    <SidebarMenu>
+                        {items.map((item) => (
+                            <SidebarMenuItem key={item.title}>
+                                <SidebarMenuButton asChild>
+                                    <Link href={item.url}>
+                                        <item.icon />
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+
+            <SidebarSeparator/>
+
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <SidebarMenuButton>
+                                    <User2/> Kristiyan Kirilov <ChevronUp className="ml-auto"/>
+                                </SidebarMenuButton>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                                <DropdownMenuItem>Acount</DropdownMenuItem>
+                                <DropdownMenuItem>Settings</DropdownMenuItem>
+                                <DropdownMenuItem>Sign Out</DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
+
+        </Sidebar>
+    )
+}
